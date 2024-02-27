5735a59d1071838a61c334bb892c408b79ef24fe
shubham <shubhamsagar628@gmail.com>
2024-02-27 10:35:44 +0530

    folder structure created

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import HomePage from './pages/home/HomePage'
+import NoPage from './pages/noPage/NoPage'
 
 
 function App() {
  
+  const appRouter = createBrowserRouter([
+    {
+      path:'/',
+      element:<HomePage/>
+    },
+    {
+      path:'/*',
+      element:<NoPage/>
+    }
+  ])
 
 return (
   <div>
-<h2 className='bg-red-100'>hare krishana</h2>
+    <RouterProvider router={appRouter} />
   </div>
 )
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>Navbarss</div>
+  )
+}
+
+export default Navbar
