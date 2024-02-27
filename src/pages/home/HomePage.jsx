77c457300365c4a28c34a9809a34890f56ec9f40
shubham <shubhamsagar628@gmail.com>
2024-02-27 11:28:15 +0530

    Creating HeroSection

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../../components/layout/Layout'
+import HeroSection from '../../components/heroSection/HeroSection'
 
 const HomePage = () => {
   return (
     <Layout>
-        home HomePage
+   <HeroSection/>
     </Layout>
   )
 }
